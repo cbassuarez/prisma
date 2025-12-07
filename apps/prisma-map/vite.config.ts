92864ed3138d156f4344caa90941e411f5641a81
Seb Suarez <contact@cbassuarez.com>
2025-12-07 15:59:32 -0800

    fix brach base

--- apps/prisma-map/vite.config.ts
+++ apps/prisma-map/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/prisma-map/",
+  base: "/prisma/",
   resolve: {
     alias: {
       '@prisma/config': path.resolve(__dirname, '../../packages/prisma-config/src')
